178aeaeaf817543ec8de6bb9658d6fe229a05c55
sahariar8 <sahariaralam8@gmail.com>
2025-06-17 21:49:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-3xl font-bold text-rose-600'>React</h1>
+     
+     
+    </>
+  )
+}
+
+export default App
